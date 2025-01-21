9edb134e62f7577521f521790f4c013c831d7a22
Gibran Khalil <gibrankhalil150904@gmail.com>
2025-01-21 19:12:53 -0300

    Navegação pelo menu inferior

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home/home.component';
+import { MembersPage } from './pages/members/members.component';
+import { MinisterysPage } from './pages/ministerys/ministerys.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePage
+  },
+  {
+    path: 'members',
+    component: MembersPage
+  },
+  {
+    path: 'ministerys',
+    component: MinisterysPage
   }
 ];
--- src/ui/components/bottom-nav-bar/bottom-nav-bar.component.ts
+++ src/ui/components/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Calendar, House, LucideAngularModule, SquareUserRound, UsersRound } from 'lucide-angular';
 
 @Component({
   selector: 'app-bottom-nav-bar',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterModule],
   templateUrl: './bottom-nav-bar.component.html',
   styleUrl: './bottom-nav-bar.component.scss'
 })
 export class BottomNavBarComponent {
-  readonly Icons = [House, Calendar, UsersRound, SquareUserRound];
+  readonly Icons = [
+    {
+      icon: House,
+      path: '/'
+    },
+    {
+      icon: Calendar,
+      path: '/calendar'
+    },
+    {
+      icon: UsersRound,
+      path: '/members'
+    },
+    {
+      icon: SquareUserRound,
+      path: '/profile'
+    }
+  ];
 }
