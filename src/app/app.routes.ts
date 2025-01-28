d27c9d53318e1236905edc886faa2b14798e2ce6
Gibran Khalil <gibrankhalil150904@gmail.com>
2025-01-28 17:17:48 -0300

    Tela de novos ministérios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,12 @@ import { MembersPage } from './pages/members/members.component';
 import { MinisterysPage } from './pages/ministerys/ministerys.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { NewMemberPage } from './pages/members/new/new.component';
-<<<<<<< HEAD
-import { DayScaleMinisteryComponent } from './pages/day-scale-ministery/day-scale-ministery.component';
-=======
 import { NewMinisteryPage } from './pages/ministerys/new/new.component';
->>>>>>> d7cee4bcdee9b4edcdb5092777267940f3744578
 
 export const routes: Routes = [
   {
     path: '',
-    component: DayScaleMinisteryComponent
+    component: HomePage
   },
   {
     path: 'calendar',
--- src/app/pages/ministerys/new/new.component.ts
+++ src/app/pages/ministerys/new/new.component.ts
@@ -3,10 +3,11 @@ import { TitleComponent } from '../../../../ui/components/title/title.component'
 import { TextInputComponent } from '../../../../ui/components/inputs/text-input/text-input.component';
 import { FileInputComponent } from '../../../../ui/components/inputs/file-input/file-input.component';
 import { UserInputComponent } from '../../../../ui/components/inputs/user-input/user-input.component';
+import { ButtonComponent } from '../../../../ui/components/button/button.component';
 
 @Component({
   selector: 'app-new',
-  imports: [TitleComponent, TextInputComponent, FileInputComponent, UserInputComponent],
+  imports: [TitleComponent, TextInputComponent, FileInputComponent, UserInputComponent, ButtonComponent],
   templateUrl: './new.component.html',
   styleUrl: './new.component.scss'
 })
