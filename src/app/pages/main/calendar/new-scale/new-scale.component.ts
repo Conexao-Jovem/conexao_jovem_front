7cee6d0df59c3ee27ccb3a465b3602b6036d9729
Gibran Khalil <gibrankhalil150904@gmail.com>
2025-02-13 08:02:11 -0300

    Adição de página para criação de escala - estilização

--- src/app/pages/main/calendar/new-scale/new-scale.component.ts
+++ src/app/pages/main/calendar/new-scale/new-scale.component.ts
@@ -2,13 +2,37 @@ import { Component } from '@angular/core';
 import { TitleComponent } from '../../../../../ui/components/title/title.component';
 import { ButtonComponent } from '../../../../../ui/components/button/button.component';
 import { LucideAngularModule, X } from 'lucide-angular';
+import { DateInputComponent } from '../../../../../ui/components/inputs/date-input/date-input.component';
+import { Scale } from '../../../../../data/services/scales/@types/find.dto';
+import { SelectInputComponent } from '../../../../../ui/components/inputs/select-input/select-input.component';
 
 @Component({
   selector: 'app-new-scale',
-  imports: [TitleComponent, ButtonComponent, LucideAngularModule],
+  imports: [TitleComponent, ButtonComponent, LucideAngularModule, DateInputComponent, SelectInputComponent],
   templateUrl: './new-scale.component.html',
   styleUrl: './new-scale.component.scss'
 })
 export class NewScaleComponent {
   readonly XIcon = X;
+  scales: Scale[] = [];
+
+  departmentInput: Partial<HTMLSelectElement> = {
+    name: 'department',
+    ariaPlaceholder: 'Selecione um departamento'
+  };
+
+  departmentOptions: Partial<HTMLOptionElement>[] = [
+    {
+      value: '0',
+      label: 'Selecione um departamento'
+    },
+    {
+      value: '1',
+      label: 'Louvor'
+    },
+    {
+      value: '2',
+      label: 'Diretoria'
+    }
+  ];
 }
---
+++ src/ui/components/inputs/date-input/date-input.component.ts
@@ -0,0 +1,46 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-date-input',
+  templateUrl: './date-input.component.html',
+  styleUrls: ['./date-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DateInputComponent),
+      multi: true
+    }
+  ]
+})
+export class DateInputComponent implements ControlValueAccessor {
+  @Input() placeholder: string = '';
+  @Input() disabled: boolean = false;
+  value: string = '';
+
+  private onChange: any = () => {};
+  private onTouched: any = () => {};
+
+  onInputChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
